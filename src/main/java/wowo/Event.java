4249768d4404844e2a291d028f587d7d8de8d5b7
aufasodik <aufashadiq06@gmail.com>
2025-09-03 20:39:19 +0800

    Make JavaDoc for Event.java and EmptyDescriptionException.java

--- src/main/java/wowo/Event.java
+++ src/main/java/wowo/Event.java
@@ -3,11 +3,20 @@ package wowo;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Represent an event task with start and end date
+ */
 public class Event extends Task {
     private static final DateTimeFormatter OUT_FMT = DateTimeFormatter.ofPattern("MMM d yyyy");
     private final LocalDate from;
     private final LocalDate to;
 
+    /**
+     * Creates an Event with a start and end date
+     * @param name Description of the event
+     * @param from The start date in "yyyy-MM-dd"
+     * @param to The end date in "yyyy-MM-dd"
+     */
     public Event(String name, LocalDate from, LocalDate to) {
         super(name);
         this.from = from;
